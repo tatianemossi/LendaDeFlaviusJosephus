209b85aa67a44ab00ca3f60150e97f5b6f675817
Tatiane Mossi <mossitatiane2@gmail.com>
2022-02-18 16:28:23 -0300

    Atualização Josephus

--- LendaDeFlaviusJosephus.ConsoleApp/Program.cs
+++ LendaDeFlaviusJosephus.ConsoleApp/Program.cs
@@ -6,7 +6,21 @@ namespace LendaDeFlaviusJosephus.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite o número de pessoas e o tamanho do salto (separados por espaço, como o exemplo: 5 2: ");
+            var pessoasTamanhoDoSalto = (Console.ReadLine());
+            char[] listaPessoasTamanhoDoSalto = pessoasTamanhoDoSalto.ToCharArray();
+
+            var quantidadeDePessoas = listaPessoasTamanhoDoSalto[0];
+            var tamanhoDoSalto = listaPessoasTamanhoDoSalto[2];
+
+            for (int i = 0; i < listaPessoasTamanhoDoSalto.Length; i++)
+            {
+                if ()
+                {
+
+                }
+            }
+
         }
     }
 }
